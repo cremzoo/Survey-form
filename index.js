ed5c47f8abe9ccb8eee192262faea7e519217859
Cremona Matei <cremzoo@yahoo.com>
2018-11-07 18:14:13 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $(".radios").append(
+    '<input type="radio" name="check" value="yes">Yes' +
+      "<br>" +
+      '<input type="radio" name="check" value="no">No' +
+      "<br>" +
+      '<input type="radio" name="check" value="no">Do not know'
+  );
+});
